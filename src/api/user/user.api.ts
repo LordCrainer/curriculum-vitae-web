35896e70a64d7a9d030ed4e598a921809d3a6088
LordCrainer <camogan3000@hotmail.com>
2022-11-24 01:41:21 -0500

    volviendo al juego

--- src/api/user/user.api.ts
+++ src/api/user/user.api.ts
@@ -3,7 +3,13 @@ import { api } from 'src/modules/axios'
 import { AxiosResponse } from 'axios'
 
 export const userApi = {
-  get: async (id: number) => api.get(`/api/users/${id}`),
+  get: async (id: number) => {
+    try {
+      return api.get(`/api/users/${id}`)
+    } catch (error) {
+      console.error('userApi.get: ', error)
+    }
+  },
   login: (obj: any) => api.post('/api/login', obj),
   create: (obj: any) => api.post('/api/users', obj),
   update: (obj: any) => api.put('/api/users', obj),
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="column flex-center pa-4">
-    <div class="w-50% text-right">
-      <q-select v-model="locale" :options="availableLocales" label="choose your language">
-        <template v-slot:prepend>
-          <q-icon :name="matLanguage" />
-        </template>
-      </q-select>
-    </div>
+  <div class="column">
+    <div class="column flex-center pa-4">
+      Prueba {{ count }}
 
-    <img class="pa-5" alt="Vue logo" src="/src/assets/img/logo.png" @click="getUser" />
-    <HelloWorld :msg="t('pages.index.')" />
-    <span class="accent-color"> 22 </span>
-    {{ count }}
-    <AsyncInput v-model="count" />
+      <img class="pa-5" alt="Vue logo" src="/src/assets/img/logo.png" @click="getUser" />
+    </div>
+    <div class="container">
+      <svg
+        class="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shape-rendering="auto">
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g class="parallax">
+          <use xlink:href="#gentle-wave" x="25" y="0" fill="#fd6601" />
+          <use xlink:href="#gentle-wave" x="55" y="3" fill="#08365b" />
+        </g>
+      </svg>
+    </div>
   </div>
 </template>
 
@@ -28,20 +39,89 @@
 
   // import CustomInput from 'src/components/CustomInput.vue'
   //Async Components
-  const AsyncInput = defineAsyncComponent(() => import('src/components/CustomInput.vue'))
+  // const AsyncInput = defineAsyncComponent(() => import('src/components/CustomInput.vue'))
 
   const { t, locale, availableLocales } = useI18n()
 
   const emit = defineEmits(['update'])
+  const count = ref<number>(0)
 
-  const getUser = (): void => {
-    userApi.get(2).then((res) => {
-      const user: User = res.data
-      emit('update', user)
-    })
+  const getUser = async () => {
+    count.value++
+    // await userApi.get(2).then((res) => {
+    //   const user: User = res.data
+    //   console.log('user', user)
+    //   emit('update', user)
+    // })
   }
-
-  const count = ref<string | null>(null)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .container {
+    position: relative;
+    text-align: center;
+    // background: green;
+    color: black;
+    width: 100%;
+  }
+  .waves {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin-bottom: -7px; /*Fix for safari gap*/
+    min-height: 50px;
+    max-height: 100px;
+  }
+  .wave-1 {
+    background-color: $primary;
+  }
+  .wave-2 {
+    background-color: $primary;
+  }
+  .wave-3 {
+    background-color: $primary;
+  }
+
+  .parallax > use {
+    animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+  }
+  .parallax > use:nth-child(1) {
+    animation-delay: -1s;
+    animation-duration: 7s;
+  }
+  .parallax > use:nth-child(2) {
+    animation-delay: -3s;
+    animation-duration: 10s;
+  }
+  // .parallax > use:nth-child(3) {
+  //   animation-delay: -4s;
+  //   animation-duration: 13s;
+  // }
+  // .parallax > use:nth-child(4) {
+  //   animation-delay: -5s;
+  //   animation-duration: 20s;
+  // }
+  @keyframes move-forever {
+    0% {
+      transform: translate3d(-90px, 0, 0);
+    }
+    100% {
+      transform: translate3d(85px, 0, 0);
+    }
+  }
+
+  /*Shrinking for mobile*/
+  @media (max-width: 768px) {
+    .waves {
+      height: 40px;
+      min-height: 40px;
+      width: 100%;
+    }
+    // .content {
+    //   height:30vh;
+    // }
+    // h1 {
+    //   font-size:24px;
+    // }
+  }
+</style>
